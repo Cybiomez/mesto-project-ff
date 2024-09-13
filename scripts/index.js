bbb666196e2257925537170d475db5d62c835e40
Cybiomez <cybiomez@yandex.ru>
2024-09-13 15:45:46 +0300

    i_dont_know_how_do_it

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
+function addCard (card_add, card_remove) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.song').cloneNode(true);
+
+    cardContainer.append(cardElement);
+    return(card);
+}
+
+
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
